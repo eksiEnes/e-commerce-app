f9889d7059d7a08179c146182bdc15ad67166d6c
Enes Ekşi <enes-eksi@hotmail.com>
2024-09-06 13:49:52 +0300

    feat(product-service): add .gitignore , application.yml

---
+++ services/product/src/main/java/com/enes/product/product/Product.java
@@ -0,0 +1,32 @@
+package com.enes.product.product;
+
+import com.enes.product.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private double availableQuantity;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
